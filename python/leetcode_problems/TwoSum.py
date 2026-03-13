76a08431dfe970724324478b4c0f6b38d3ae251b
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2026-03-13 15:38:02 +0530

    refresh

--- python/leetcode_problems/TwoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap: dict = dict()
-        for i in range(len(nums)):
-            num: int = target - nums[i]
-            if(num in hashMap):
-                return [i, hashMap[num]]
-            hashMap[nums[i]] = i
-        return None
